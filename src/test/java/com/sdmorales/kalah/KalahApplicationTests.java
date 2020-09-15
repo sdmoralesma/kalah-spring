cf01463fde2f3d25d20c7409d06d07f3ccae038f
Sergio Morales <sergio@backbase.com>
2020-09-15 21:17:02 +0200

    code reindent

--- src/test/java/com/sdmorales/kalah/KalahApplicationTests.java
+++ src/test/java/com/sdmorales/kalah/KalahApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KalahApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
