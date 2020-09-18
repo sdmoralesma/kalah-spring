8744a868f598f18947b2444280da2254c7869114
Sergio Morales <sergio@backbase.com>
2020-09-18 21:04:37 +0200

    remove unnecessary code

--- kalah-service/src/main/java/com/sdmorales/kalah/KalahRepository.java
+++ kalah-service/src/main/java/com/sdmorales/kalah/KalahRepository.java
@@ -1,10 +1,7 @@
 package com.sdmorales.kalah;
 
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface KalahRepository extends CrudRepository<Game, Long> {
-
-    List<Game> findAll();
 
 }
